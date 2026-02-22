527a9a062ff151671f9c373dd8de4f77d052b841
Dharahas04 <dharahas04@gmail.com>
2026-02-22 23:59:11 +0530

    initial commit - spring boot project setup

---
+++ src/test/java/com/medtrack/medtrack_backend/MedtrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medtrack.medtrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedtrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
