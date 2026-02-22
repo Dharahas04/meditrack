7aa4c4a8d868ed19d07e3e83d29fb95f3c97ddca
Dharahas04 <dharahas04@gmail.com>
2026-02-23 00:08:43 +0530

    added Department entity

---
+++ src/main/java/com/meditrack/meditrack_backend/model/Department.java
@@ -0,0 +1,23 @@
+package com.meditrack.meditrack_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String floor;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
