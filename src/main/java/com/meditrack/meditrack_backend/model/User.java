b86e20a63bbe9c1222a7eea83f15732a47117c2f
Dharahas04 <dharahas04@gmail.com>
2026-02-23 00:09:34 +0530

    added User entity

---
+++ src/main/java/com/meditrack/meditrack_backend/model/User.java
@@ -0,0 +1,42 @@
+package com.meditrack.meditrack_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    private Shift shift;
+
+    private String phone;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = L
